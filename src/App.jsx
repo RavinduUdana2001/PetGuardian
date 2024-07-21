72efab789bf1155802202c257646b0e457a9d0ce
RavinduUdana2001 <ravindusinghapura@gmail.com>
2024-07-21 14:42:52 +0530

    Update one

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,11 @@ import HeaderContent from './Components/HeaderContent/HeaderContent'
 import BodyContent from './Components/BodyContent/BodyContent';
 import SecondContent from './Components/SecondContent/SecondContent';
 import FooterContent from './Components/FooterContent/FooterContent';
+import DetailContent from './Components/DetailContent/DetailContent';
+import PetCategories from './Components/PetCategories/PetCategories';
+import DescriptionSection from './Components/DescriptionSection/DescriptionSection';
+import BookAConsultation from './Components/BookAConsultation/BookAConsultation';
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,7 +21,11 @@ function App() {
       
       <HeaderContent/>
       <BodyContent/>
+      <DetailContent/>
       <SecondContent/>
+      <PetCategories/>
+      <BookAConsultation/>
+      <DescriptionSection/>
       <FooterContent/>
       
     </>
