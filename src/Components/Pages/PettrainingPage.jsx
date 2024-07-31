b14b303ca28300da75bb33bb1ea3303e5131b9dc
RavinduUdana2001 <ravindusinghapura@gmail.com>
2024-07-31 17:57:21 +0530

    pet training 1

--- src/Components/Pages/PettrainingPage.jsx
+++ src/Components/Pages/PettrainingPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HeaderContent from '../HeaderContent/HeaderContent'
-import DescriptionSection from '../DescriptionSection/DescriptionSection'
+import Pt1Content from '../Pt1Content/Pt1Content'
+import Pt2Content from '../Pt2Content/Pt2Content'
 
 function PettrainingPage() {
   return (
     <div>
         <HeaderContent/>
-        <DescriptionSection/>
+        <Pt1Content/>
+        <Pt2Content/>
     </div>
   )
 }
