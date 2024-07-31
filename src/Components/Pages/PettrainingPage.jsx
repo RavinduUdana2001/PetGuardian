d5017a3a9ad46a26135d05c3837486b9d76dee1d
RavinduUdana2001 <ravindusinghapura@gmail.com>
2024-07-31 18:13:48 +0530

    pet training 2

--- src/Components/Pages/PettrainingPage.jsx
+++ src/Components/Pages/PettrainingPage.jsx
@@ -6,7 +6,7 @@ import Pt2Content from '../Pt2Content/Pt2Content'
 function PettrainingPage() {
   return (
     <div>
-        <HeaderContent/>
+        
         <Pt1Content/>
         <Pt2Content/>
     </div>
